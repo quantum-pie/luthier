97026208426543db699cf4e53d593db797b2e8d3
Pavel Ermakov <big.awesome.pie@gmail.com>
2025-04-24 23:05:01 +0200

    Cluster songs tags and classify songs

--- src/composition/training/training_data/lakh/parser_runner.py
+++ src/composition/training/training_data/lakh/parser_runner.py
@@ -1,12 +1,13 @@
 import click
 import logging
-from src.composition.training.training_data.lakh.loader import LakhLoader
+from src.composition.training.training_data.lakh.parser import LakhParser
 from pathlib import Path
+import os
 
 @click.command()
 @click.option("--dataset_dir", type=Path, required=True)
 def cli(dataset_dir):
-    LakhLoader(dataset_dir)
+    LakhParser(dataset_dir)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
