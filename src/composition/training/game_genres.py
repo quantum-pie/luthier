4aa691ff91bd8d26731ea5a5a6cfe263cc82cf1c
Pavel Ermakov <big.awesome.pie@gmail.com>
2025-04-25 16:03:50 +0200

    Generate mapping from clusters to game genres

---
+++ src/composition/training/game_genres.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class GameGenres(Enum):
+    FANTASY = "fantasy"
+    CYBERPUNK = "cyberpunk"
+    SCI_FI = "sci-fi"
+    HISTORICAL = "historical"
+    HORROR = "horror"
+    POST_APOCALYPTIC = "post-apocalyptic"
+    NOIR = "noir"
+    RETRO = "retro"
+    DARK_FAIRY_TALE = "dark fairy tale"
+    MODERN_URBAN = "modern urban"
+    SURREAL = "surreal"
+    WESTERN = "western"
+    WHIMSICAL = "whimsical"
+    # Add more genres as needed
---
+++ src/composition/training/training_data/midi_parser.py
@@ -0,0 +1,11 @@
+class MidiParser:
+    def __init__(self, midi_file):
+        self.midi_file = midi_file
+        self.parsed_data = None
+
+    def parse(self):
+        # Implement MIDI parsing logic here
+        pass
+
+    def get_parsed_data(self):
+        return self.parsed_data
