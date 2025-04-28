d66676ceb81bafc7b69970d586ae814c056932dc
Pavel Ermakov <big.awesome.pie@gmail.com>
2025-04-28 17:57:46 +0200

    Add simple MIDI tagger

---
+++ src/composition/game_moods.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class GameMoods(Enum):
+    CALM = "calm"
+    TENSE = "tense"
+    DARK = "dark"
+    SAD = "sad"
+    MYSTERIOUS = "mysterious"
+    HOPEFUL = "hopeful"
+    EPIC = "epic"
+    ENERGETIC = "energetic"
+    CHAOTIC = "chaotic"
+    AGGRESSIVE = "aggressive"
+    EERIE = "eerie"
+    TERRIFYING = "terrifying"
+    WHIMSICAL = "whimsical"
+    LONELY = "lonely"
+    MAGICAL = "magical"
